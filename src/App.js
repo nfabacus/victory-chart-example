3a73745bc529af93edd4a5c6c9a43417951aaff1
Noby Fujioka <n.fujioka@abacusmaths.info>
2018-12-15 09:20:43 +0000

    victory chart first example bar chart

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { VictoryBar, VictoryChart, VictoryTheme, VictoryLabel, VictoryPortal, VictoryAxis } from 'victory';
 import './App.css';
+
+const data = [
+  { topic: "topic fdsafsAbcd", points: 80},
+  {topic: "topic fsda B", points: 24},
+  {topic: "topic C", points: 6},
+  {topic: "topic D", points: 50}
+];
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
+        <h1 style={{textAlign: "center"}}>Victory Chart Example</h1>
+        <div style={{ maxWidth: "800px", margin: "auto" }}>
+          <VictoryChart
+            theme={VictoryTheme.material}
+            domainPadding={20}
+            padding={{ left: 90, top: 50, right: 10, bottom: 50 }}
           >
-            Learn React
-          </a>
-        </header>
+            <VictoryAxis
+              dependentAxis
+              tickLabelComponent={<VictoryPortal><VictoryLabel/></VictoryPortal>} />
+            <VictoryAxis
+              tickLabelComponent={<VictoryPortal><VictoryLabel/></VictoryPortal>} />
+            <VictoryBar horizontal
+                        data={data}
+                        y="points"
+                        x="topic"
+            />
+          </VictoryChart>
+        </div>
+
       </div>
     );
   }
